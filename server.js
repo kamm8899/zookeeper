7786e8f9df056270172f7be8ffba81a7dd3313a7
kamm8899 <kamm8899@gmail.com>
2021-08-27 13:30:53 -0700

    more code

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const path = require('path');
+const { addAbortSignal } = require('stream');
 
 //parse incoming string or array data 
 app.use(express.urlencoded({ extended: true}));
 //parse incoming JSON data
 app.use(express.json());
+
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -110,6 +113,21 @@ app.post('/api/animals', (req,res) => {
     res.json(req.body);
   }
 });
+
+app.get('/', (req, res) =>{
+  res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+})
+
+app.get('/zookeepers', (req, res) =>{
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+})
+app.get('*', (req, res) =>{
+  res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 app.listen(PORT, ()=>{
     console.log(`API server now on port ${PORT}!`);
